97b987cd895056c53203a161f75e10c772c24d2d
Nick <nick.korotzer@gmail.com>
2023-04-13 11:06:14 -0700

    basic echo functionality

--- client.py
+++ client.py
@@ -1,7 +1,15 @@
-import sys
+import socket
 
 def main():
-	pass
+    HOST = "127.0.0.1"
+    PORT = 65432
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+        s.sendall(b"Hi Server")
+        data = s.recv(1024)
+        
+    print(f"Received {data!r}")
 	
 	
 if __name__ == "__main__":
