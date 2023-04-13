4178f75683659b4ff80a615ac27a0f274ccc0fec
Nick <nick.korotzer@gmail.com>
2023-04-13 11:46:12 -0700

    added loops so the programs don't close after one message

--- client.py
+++ client.py
@@ -4,12 +4,17 @@ def main():
     HOST = "127.0.0.1"
     PORT = 65432
     
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
-        s.sendall(b"Hi Server")
-        data = s.recv(1024)
+    while(True):
+        msg = input('Write to server: ')
+        if msg == "exit":
+             break
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST,PORT))
+            s.sendall(msg.encode())
+            data = s.recv(1024)
+            
         
-    print(f"Received {data!r}")
+        print(f"Received {data.decode()}")
 	
 	
 if __name__ == "__main__":
