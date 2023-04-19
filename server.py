ac65de8dd7b820713d735ba0de7ab110a29f1c8f
Nick Korotzer <nick.korotzer@gmail.com>
2023-04-19 13:40:59 -0700

    moved call to bind and listen outside of while loop

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import socket
 def main():
     HOST = "127.0.0.1"
     PORT = 65432
+    s.bind((HOST, PORT))
+    s.listen()
     
     while(True):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
+
             conn, addr = s.accept()
             with conn:
                 print(f"Connected by {addr}")
