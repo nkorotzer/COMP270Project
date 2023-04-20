1d072ec3dc868cc34b9a03f14fc68965e63990d8
Nick <nick.korotzer@gmail.com>
2023-04-20 11:34:58 -0700

    basic threading functionality implemented

--- client.py
+++ client.py
@@ -1,20 +1,29 @@
+import sys
 import socket
 
 def main():
     HOST = "127.0.0.1"
     PORT = 65432
-    
-    while(True):
-        msg = input('Write to server: ')
-        if msg == "exit":
-             break
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        try:
             s.connect((HOST,PORT))
+        except:
+            print('Error connecting, closing...')
+            sys.exit()
+
+        print(f'Connected to {HOST}:{PORT}')
+        while(True):
+            msg = input('Write to server: ')
+            if msg == "exit" or not msg:
+                print('Closing connection...')
+                break
+            
             s.sendall(msg.encode())
             data = s.recv(1024)
-            
-        
-        print(f"Received {data.decode()}")
+                
+            print(f"Received {data.decode()}")
+        sys.exit()
 	
 	
 if __name__ == "__main__":
