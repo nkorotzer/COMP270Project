db7f3efb738f209ec900a47c044bd9b0269bbda9
Nick <nick.korotzer@gmail.com>
2023-05-09 21:50:33 -0700

    added encrypt and decrypt functions

--- encrypt.py
+++ encrypt.py
@@ -2,6 +2,19 @@
 # from Crypto.Random import get_random_bytes
 
 import nacl.pwhash
+from nacl.public import Box
+import nacl.exceptions
+
+def get_box(pub_key, sec_key):
+    return Box(sec_key, pub_key)
+
+def encrypt_text(pub_key, sec_key, ptext):
+    box = get_box(pub_key, sec_key)
+    return box.encrypt(ptext)
+
+def decrypt_text(pub_key, sec_key, ctext):
+    box = get_box(pub_key, sec_key)
+    return box.encrypt(ctext)
 
 def main():
     # following example from https://pycryptodome.readthedocs.io/en/latest/src/protocol/kdf.html#scrypt-func
